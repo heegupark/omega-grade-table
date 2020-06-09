bb2e7b282d02e7c363c72c8f1fd88c13f71abee5
heegupark <slhead@gmail.com>
2020-06-09 16:50:41 -0700

    back to http

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const jsonServer = require('json-server');
-const https = require('https');
-const fs = require('fs');
 
 const dbPath = path.resolve(__dirname, '../database/db.json');
 const server = jsonServer.create();
@@ -11,19 +9,7 @@ const PORT = 3021;
 
 server.use(middleware);
 server.use('/api', endpoints);
-// server.listen(PORT, () => {
-//   // eslint-disable-next-line no-console
-//   console.log(`JSON Server listening on port ${PORT}\n`);
-// });
-
-const keyFile = path.join(__dirname, 'server.key');
-const certFile = path.join(__dirname, 'server.cert');
-
-https.createServer({
-  key: fs.readFileSync(keyFile),
-  cert: fs.readFileSync(certFile)
-},
-server).listen(PORT, () => {
+server.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`[https] JSON Server listening on port ${PORT}`);
+  console.log(`[http] JSON Server listening on port ${PORT}\n`);
 });
